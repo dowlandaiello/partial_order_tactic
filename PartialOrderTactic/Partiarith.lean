18bbfa0583831ba4f42bc5aa242b71fab7157269
dhruvbhatia00 <61220727+dhruvbhatia00@users.noreply.github.com>
2025-03-05 17:05:33 -0800

    Mar 5

--- PartialOrderTactic/Partiarith.lean
+++ PartialOrderTactic/Partiarith.lean
@@ -60,30 +60,44 @@ def parseContext (only: Bool) (hyps: Array Expr) (tgt: Expr) :
         out ← processHyp (← inferType hyp) out
     pure (e₁, e₂, out)
 
-def dfs (v₁ : Expr) (v₂ : Expr) (edges : Array (Expr × Expr))
-    : AtomM (Array (Expr × Expr)) := do
-    -- match edges with
-    -- | v₁ × g => logInfo f!"{g}"
-    -- | _ => logInfo f!""
 
-    let rec
-    getNeighbors (tgt: Expr) (e: Array (Expr × Expr)) (out: Array (Expr × Expr)) := do
-      for edge in e do
-        if ← isDefEq edge.1 tgt then
-          logInfo f!"{← delab edge.1}"
-          logInfo f!"{← delab edge.2}"
-          return out.push edge
-      pure out
+structure dfs_data where
+  path_so_far : Array (Expr × Expr)
+  discovered : Array (Expr)
+
+def dfs_outer (v₁ : Expr) (v₂ : Expr) (edges : Array (Expr × Expr))
+    : StateT dfs_data MetaM (Option Unit) := do
+
+  let rec
+  getNeighbors (tgt: Expr) := do
     let mut out := #[]
-    out ← getNeighbors v₁ edges out
-    pure out
+    for edge in edges do
+      if ← isDefEq edge.1 tgt then
+          -- logInfo f!"{← delab edge.1}"
+          -- logInfo f!"{← delab edge.2}"
+        return out.push edge
+    return out
 
+  let rec
+  dfs_loop (node : Expr) := do
+    let neighbors ← getNeighbors node
+    let st ← get
+    let updated_discovered ← pure {st with discovered := st.discovered.push node}
+    if neighbors.size == 0 then
+      let updated_path_so_far ← pure {st with path_so_far:= st.path_so_far.extract 0 (st.path_so_far.size - 1)}
+
+
+
+    for neighbor in neighbors do
+
+  dfs_loop v₁
+#check Meta.State
 
 def partiarith (g : MVarId) (only : Bool) (hyps : Array Expr)
     (traceOnly := false) : MetaM (Except MVarId (Unit)) := do
     g.withContext <| AtomM.run .reducible do
     let (v₁, v₂, edges) ← parseContext only hyps (← g.getType)
-    let relevant_edges ← dfs v₁ v₂ edges
+    let relevant_edges ← dfs_outer v₁ v₂ edges
     -- logInfo f!"{← delab v₁}"
     -- logInfo f!"{← delab v₂}"
     -- for edge in edges do
